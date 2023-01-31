f5a2209b3d026ce17acf4072851baa9f98c3405f
Kévin Maublanc <thekeke60@gmail.com>
2023-01-31 14:38:18 +0100

    SiteWeb main Added

---
+++ assets/js/accueil.js
@@ -0,0 +1,7 @@
+function pageInit()
+{
+    var leftLabel = document.querySelector('.leftLabel');
+    var rightLabel = document.querySelector('.rightLabel');
+
+    
+}
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kévin Maublanc | PortFolio</title>
+    
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <script src="assets/js/accueil.js"></script>
+
+    <link rel="stylesheet" href="assets/css/accueil.css">
+</head>
+<body onload="pageInit()">
+    <header>
+        <ul class="navbar">
+            <li><a href="#" class="nav-item">Accueil</a></li>
+            <li><a href="#" class="nav-item">Réalisations</a></li>
+            <li><a href="#" class="nav-item">JeSaisPlus</a></li>
+            <li><a href="#" class="nav-item">Bilan</a></li>
+            <li><a href="#" class="nav-item">Veille</a></li>
+        </ul>
+    </header>
+    <div id="mainContener">
+        <div class="leftLabel">
+            <h1>Kévin 'SilverHawks' Maublanc</h1>
+            <p>Je suis un apprenti en programmation informatique en BTS SIO SLAM. je suis passionnée par la programmation depuis plusieurs année et je souhaite en faire mon métier. J'ai commencé à l'âge de 10 ans à m'y interesser mais j'ai commencé à réelement pratiqué et apprendre vers 14 ans.</p>
+        </div>
+        <img src="assets/img/purplehinata.png" alt="Photo Kévin" class="rightLabel" />
+
+
+    </div>
+
+    <footer>
+        Kévin 'SilverHawks' Maublanc &copy; 2023
+    </footer>
+</body>
+</html>
